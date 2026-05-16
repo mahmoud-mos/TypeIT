87180875acbd452c66078e0679bcd15dcc213c27
Mahmoud <mhmoud.mostafa123@gmail.com>
2026-05-16 03:42:22 +0300

    Main Engine to run the App

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+
+// the true entry point of the app
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(ui.ChatApplication   .class, args);
+    }
+}
